eca4d1c75ec28e4a339f924208b5dbf35f34ec69
Isaiah Akorita <akoritaisaiah@gmail.com>
2025-05-28 12:26:28 +0100

    add releases job to workflow files

--- scripts/process_repos.py
+++ scripts/process_repos.py
@@ -13,6 +13,7 @@ for name, repo in REPOS.items():
     json_in = f"data/{name}_metrics.json"
     json_out = f"data/{name}_updated.json"
     parquet_out = f"data/{name}_metrics.parq"
+    csv_out = f"data/{name}_releases.csv"
 
     subprocess.run(
         ["python", "scripts/update_issues.py", json_in, repo, json_out], check=True
@@ -20,3 +21,4 @@ for name, repo in REPOS.items():
     subprocess.run(
         ["python", "scripts/convert_json.py", json_out, parquet_out], check=True
     )
+    subprocess.run(["python", "scripts/get_releases.py", repo, csv_out], check=True)
